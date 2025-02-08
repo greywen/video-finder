d8210db53e9f1dcf5e51339f75ba15b25dd5a788
gy <gray.wen@outlook.com>
2025-02-08 17:20:07 +0800

    成功接入ollama

--- pages/api/analyze.ts
+++ pages/api/analyze.ts
@@ -2,6 +2,13 @@ import { promises as fs } from 'fs';
 import { NextApiRequest, NextApiResponse } from 'next';
 import ffmpeg from 'fluent-ffmpeg';
 import path from 'path';
+import { MessageRole } from '@/types/common';
+
+export const config = {
+  api: {
+    responseLimit: false,
+  },
+}
 
 async function fileExists(filePath: string) {
   try {
@@ -18,7 +25,7 @@ interface GetVideoKeyframeImgParams {
   imgOutputPath: string;
 }
 
-async function getVideoKeyframeImg(params: GetVideoKeyframeImgParams) {
+async function getVideoKeyframeImg(params: GetVideoKeyframeImgParams): Promise<string> {
   const { videoFilePath, seek, imgOutputPath } = params;
   const imgFileOutputPath = path.join(imgOutputPath, seek + '.png');
   return new Promise((resolve, reject) => {
@@ -57,9 +64,16 @@ function createImgOutputDirectory(fileNameWithExt: string) {
   return outputPath;
 }
 
+async function convertImageFileToBase64(filePath: string) {
+  const imagePath = path.resolve(filePath);
+  const imageBuffer = await fs.readFile(imagePath);
+  return imageBuffer.toString('base64');
+}
+
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
-    const { fileNameWithExt } = req.body;
+    // const { fileNameWithExt } = req.body;
+    const fileNameWithExt = 'car.mp4'
     const videoFilePath = `public/videos/` + fileNameWithExt;
     if (!(await fileExists(videoFilePath))) {
       res.json({ message: '视频文件不存在' });
@@ -71,6 +85,59 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       videoFilePath,
       seek: 10,
     });
-    res.json({ data: { imageFilePath } });
+
+    const base64Image = await convertImageFileToBase64(imageFilePath);
+
+    const url = 'http://localhost:11434/api/chat';
+
+    const response = await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({
+        model: 'llama3.2-vision:latest',
+        messages: [{ role: MessageRole.User, images: [base64Image], content: '图片中有什么？' }],
+      })
+    });
+
+    const decoder = new TextDecoder();
+    let buffer = '';
+
+    const reader = response.body?.getReader();
+    if (!reader) {
+      throw new Error('Response body is null or not readable');
+    }
+
+    while (true) {
+      const { done, value } = await reader.read();
+      if (done) {
+        break;
+      }
+
+      buffer += decoder.decode(value, { stream: true });
+
+      let lines = buffer.split('\n');
+      buffer = lines.pop() || '';
+
+      for (const line of lines) {
+        if (line.trim() !== '') {
+          const json = JSON.parse(line);
+          if (json.done) {
+            res.end();
+            return;
+          }
+          if (json?.error) {
+            throw json.error;
+          }
+          const text = json.message.content || '';
+          res.write(
+            Buffer.from(
+              `data: ${JSON.stringify({
+                result: text,
+                success: true,
+              })}\r\n\r\n`,
+            ),
+          );
+        }
+      }
+    }
   }
 };
--- server.ts
+++ server.ts
@@ -1,6 +1,7 @@
-import { createServer } from 'http';
 import { parse } from 'url';
 import next from 'next';
+import express from "express"
+import path from 'path';
 
 const port = parseInt(process.env.PORT || '3000', 10);
 const dev = process.env.NODE_ENV !== 'production';
@@ -8,14 +9,20 @@ const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  createServer((req, res) => {
-    const parsedUrl = parse(req.url!, true);
-    handle(req, res, parsedUrl);
-  }).listen(port);
+  const server = express();
 
-  console.log(
-    `> Server listening at http://localhost:${port} as ${
-      dev ? 'development' : process.env.NODE_ENV
-    }`
-  );
+  server.use(express.static(path.join(__dirname, 'public')));
+
+  server.all('*', (req, res) => {
+    const parsedUrl = parse(req.url!, true);
+    return handle(req, res, parsedUrl);
+  });
+
+  server.listen(port, (error) => {
+    if (error) throw error;
+    console.log(
+      `> Server listening at http://localhost:${port} as ${dev ? 'development' : process.env.NODE_ENV
+      }`
+    );
+  })
 });
